3ac819abe741425b1e221327228136eb81ac6089
DELL <DELL@192.168.1.18>
2018-12-19 22:06:49 +0200

    basic world

--- src/eg/edu/alexu/csd/oop/game/myGame/model/CircusOfPlates.java
+++ src/eg/edu/alexu/csd/oop/game/myGame/model/CircusOfPlates.java
@@ -1,5 +1,6 @@
 package eg.edu.alexu.csd.oop.game.myGame.model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import eg.edu.alexu.csd.oop.game.GameObject;
@@ -7,34 +8,47 @@ import eg.edu.alexu.csd.oop.game.World;
 
 public class CircusOfPlates implements World {
 
+	private final int width;
+	private final int height;
+	private final List<GameObject> constant = new LinkedList<GameObject>();
+	private final List<GameObject> movable = new LinkedList<GameObject>();
+	private final List<GameObject> controlable = new LinkedList<GameObject>();
+
+	public CircusOfPlates(int screenWidth, int screenHeight) {
+		width = screenWidth;
+		height = screenHeight;
+		GameObject clown = new Clown(screenWidth/3, (int)(screenHeight)-155, "/player1.png");
+		controlable.add(clown);
+	}
+
 	@Override
 	public List<GameObject> getConstantObjects() {
 		// TODO Auto-generated method stub
-		return null;
+		return constant;
 	}
 
 	@Override
 	public List<GameObject> getMovableObjects() {
 		// TODO Auto-generated method stub
-		return null;
+		return movable;
 	}
 
 	@Override
 	public List<GameObject> getControlableObjects() {
 		// TODO Auto-generated method stub
-		return null;
+		return controlable;
 	}
 
 	@Override
 	public int getWidth() {
 		// TODO Auto-generated method stub
-		return 0;
+		return width;
 	}
 
 	@Override
 	public int getHeight() {
 		// TODO Auto-generated method stub
-		return 0;
+		return height;
 	}
 
 	@Override
