29123f32a0edafd3c09c399cb956bd028fd2c9ae
BCC <BCC@BCC-PC>
2018-12-20 17:16:58 +0200

    strategy fixed

--- src/eg/edu/alexu/csd/oop/game/myGame/controller/startegy/Easy.java
+++ src/eg/edu/alexu/csd/oop/game/myGame/controller/startegy/Easy.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package eg.edu.alexu.csd.oop.game.myGame.controller.startegy;
 
 import eg.edu.alexu.csd.oop.game.myGame.model.CircusOfPlates;
@@ -15,25 +14,4 @@ public class Easy implements Difficulty {
 	public void setDifficulty() {
 
 	}
-
 }
-=======
-package eg.edu.alexu.csd.oop.game.myGame.controller.startegy;
-
-import eg.edu.alexu.csd.oop.game.myGame.model.CircusOfPlates;
-
-public class Easy implements Difficulty {
-
-	CircusOfPlates world;
-
-	Easy(CircusOfPlates world) {
-		this.world = world;
-	}
-
-	@Override
-	public void setDifficulty() {
-
-	}
-
-}
->>>>>>> ab401e8bdda16395ce85aaa344234e398dafb54d
--- src/eg/edu/alexu/csd/oop/game/myGame/controller/startegy/Moderate.java
+++ src/eg/edu/alexu/csd/oop/game/myGame/controller/startegy/Moderate.java
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 package eg.edu.alexu.csd.oop.game.myGame.controller.startegy;
 
 import eg.edu.alexu.csd.oop.game.myGame.model.CircusOfPlates;
 
-public class Moderate implements Difficulty{
+public class Moderate implements Difficulty {
 
 	CircusOfPlates world;
 
@@ -16,22 +15,3 @@ public class Moderate implements Difficulty{
 
 	}
 }
-=======
-package eg.edu.alexu.csd.oop.game.myGame.controller.startegy;
-
-import eg.edu.alexu.csd.oop.game.myGame.model.CircusOfPlates;
-
-public class Moderate implements Difficulty{
-
-	CircusOfPlates world;
-
-	Moderate(CircusOfPlates world) {
-		this.world = world;
-	}
-
-	@Override
-	public void setDifficulty() {
-
-	}
-}
->>>>>>> ab401e8bdda16395ce85aaa344234e398dafb54d
